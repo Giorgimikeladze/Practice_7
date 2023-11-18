f74167e04ed6a7b13edbd51c6b18220ec68d552e
giorgi mikeladze <mikeladzegiorgi538@outlook.com>
2023-11-18 12:18:23 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int numb;
+Console.Write("Enter integer value: ");
+bool is_parsed = Int32.TryParse(Console.ReadLine(), out numb);
+
+if (is_parsed && numb >= 1)
+{
+    int num1 = 0;
+    int num2 = 1;
+    int num3;
+    Console.Write($"{num1} {num2} ");
+
+    for (int i = 2; i <= numb; ++i)
+    {
+        num3 = num1 + num2;
+        Console.Write($"{num3} ");
+        num1 = num2;
+        num2 = num3;
+    }
+}
+else {
+    Console.WriteLine("invalid value!!!");
+
+}
